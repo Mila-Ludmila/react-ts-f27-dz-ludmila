4951074cba3de49b33834e4c29fbf88bc7c8aad0
unknown <lydmila.briczkaya@gmail.com>
2024-03-16 14:09:47 +0200

    changes in files App.css, App.tsx,.gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import ChildComponent from "./components/ChildComponent/ChildComponent";
 
 interface ICityData {
   name: string;
@@ -43,7 +42,6 @@ function App(): JSX.Element {
   const [index, setIndex] = useState<number>(0);
 
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  // const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
   useEffect(() => {
     fetchCity();
@@ -87,104 +85,26 @@ function App(): JSX.Element {
     setIndex((prevIndex) => (prevIndex + 1) % info.photos.length);
   };
 
-  //   const menuToggle = document.querySelector('.menu-toggle');
-  // if (menuToggle) {
-  //   menuToggle.addEventListener('click', function(this: HTMLElement) {
-  //     this.classList.toggle('change'); // Додаємо/видаляємо клас 'change' до/з '.menu-toggle'
-  //   });
-  // }
-
-  // const toggleMenu = () => {
-  //   setIsMenuOpen(!isMenuOpen);
-  // };
-
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    // <div className={`App ${isMenuOpen ? 'menu-open' : ''}`}>
     <div className="App">
-<header>
-
-<div className='App-header'>
-    <label htmlFor="menu-toggle" className="hamburger-menu">
-      <input
-        id="menu-toggle"
-        type="checkbox"
-        checked={isOpen}
-        onChange={toggleMenu}
-      />
-      <span></span>
-      <span></span>
-      <span></span>
-    </label>
-    <div className={`menu ${isOpen ? "is-open" : ""}`}>
-      <h2>Погода у місті: {cityName}</h2>
-      <p>Погодні умови: {description}</p>
-      <p>{weatherStatus}</p>
-      <p className="weatherTemp">
-        Температура: {temperatureSign}
-        {temp}
-      </p>
-      <p className="weatherFeelsLike">
-        Відчувається як: {temperatureSign}
-        {feelLike}
-      </p>
-      <img
-        src={`http://openweathermap.org/img/wn/${weatherIcon}.png`}
-        alt="Weather Icon"
-      />
-    </div>
-  </div>
-
-
-        {/* <div className='App-header' >
-        <div className={`hamburger-menu ${isOpen ? "menu" : ""}`}>
-        <input
-            id="menu-toggle"
-            type="checkbox"
-            checked={isOpen}
-            onChange={toggleMenu}
-          />
-          <label htmlFor="menu-toggle">
+      <header>
+        <div className="App-header">
+          <label htmlFor="menu-toggle" className="hamburger-menu">
+            <input
+              id="menu-toggle"
+              type="checkbox"
+              checked={isOpen}
+              onChange={toggleMenu}
+            />
             <span></span>
             <span></span>
             <span></span>
           </label>
-          
-          <h2>Погода у місті: {cityName}</h2>
-          <p>Погодні умови: {description}</p>
-          <p>{weatherStatus}</p>
-          <p className="weatherTemp">
-            Температура: {temperatureSign}
-            {temp}
-          </p>
-          <p className="weatherFeelsLike">
-            Відчувається як: {temperatureSign}
-            {feelLike}
-          </p>
-          <img
-            src={`http://openweathermap.org/img/wn/${weatherIcon}.png`}
-            alt="Weather Icon"
-          />
-          </div>
-          
-        </div> */}
-
-        {/* <div className={`hamburger-menu ${isOpen ? "is-open" : ""}`}>
-          <input
-            id="menu-toggle"
-            type="checkbox"
-            checked={isOpen}
-            onChange={toggleMenu}
-          />
-          <label htmlFor="menu-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-          </label>
-          <div className="menu">
+          <div className={`menu ${isOpen ? "is-open" : ""}`}>
             <h2>Погода у місті: {cityName}</h2>
             <p>Погодні умови: {description}</p>
             <p>{weatherStatus}</p>
@@ -201,62 +121,10 @@ function App(): JSX.Element {
               alt="Weather Icon"
             />
           </div>
-        </div> */}
+        </div>
       </header>
 
-
-      {/* <header>
-        <div className="App-header">
-          <h2>Погода у місті: {cityName}</h2>
-          <p>Погодні умови: {description}</p>
-          <p>{weatherStatus}</p>
-          <p className="weatherTemp">
-            Температура: {temperatureSign}
-            {temp}
-          </p>
-          <p className="weatherFeelsLike">
-            Відчувається як: {temperatureSign}
-            {feelLike}
-          </p>
-          <img
-            src={`http://openweathermap.org/img/wn/${weatherIcon}.png`}
-            alt="Weather Icon"
-          />
-        </div>
-
-        <div className={`hamburger-menu ${isOpen ? "is-open" : ""}`}>
-          <input
-            id="menu-toggle"
-            type="checkbox"
-            checked={isOpen}
-            onChange={toggleMenu}
-          />
-          <label htmlFor="menu-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-          </label>
-          <div className="menu">
-            <h2>Погода у місті: {cityName}</h2>
-            <p>Погодні умови: {description}</p>
-            <p>{weatherStatus}</p>
-            <p className="weatherTemp">
-              Температура: {temperatureSign}
-              {temp}
-            </p>
-            <p className="weatherFeelsLike">
-              Відчувається як: {temperatureSign}
-              {feelLike}
-            </p>
-            <img
-              src={`http://openweathermap.org/img/wn/${weatherIcon}.png`}
-              alt="Weather Icon"
-            />
-          </div>
-        </div>
-      </header> */}
-
-      <div className="App-content">
+      <div className={`App-content ${isOpen ? 'blurred' : ''}`}>
         <div className="wrap-btn">
           <button className="btn btn-success" onClick={nextChange}>
             Next
@@ -269,8 +137,6 @@ function App(): JSX.Element {
         <p>Населення: {info.population} осіб</p>
         <img src={info.photos[index]} alt="info photos" />
       </div>
-
-      {/* <ChildComponent city={city} info={info} /> */}
     </div>
   );
 }
